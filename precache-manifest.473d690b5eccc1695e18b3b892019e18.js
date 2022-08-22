23e81b1e88599350e1934c03d1b5169e354b10e9
jpsahoo3 <jpsahoo3@gmail.com>
2022-08-22 08:23:31 +0530

    Updates

---
+++ precache-manifest.473d690b5eccc1695e18b3b892019e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c444b5e9fb3af3ed4043",
+    "url": "/textutil/static/css/main.40859a4f.chunk.css"
+  },
+  {
+    "revision": "c444b5e9fb3af3ed4043",
+    "url": "/textutil/static/js/main.c444b5e9.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutil/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/textutil/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "c72ea3be6a71aa226a46",
+    "url": "/textutil/static/js/runtime~main.c72ea3be.js"
+  },
+  {
+    "revision": "15b0dd52d1092f2e5cf70a9427ac93a4",
+    "url": "/textutil/index.html"
+  }
+];
